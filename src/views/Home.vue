70cce746af02b4271afac080f83304b05e13f65f
Seme Mojugbe <sememojugbe@gmail.com>
2021-07-28 21:55:19 +0100

    Added Tailwind Using https://dev.to/arielmejiadev/use-tailwind-2-with-vue-cli-2md6

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-red-500 py-12">
+    <div class="bg-white p-8 w-2/3 my-12 text-black mx-auto"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
