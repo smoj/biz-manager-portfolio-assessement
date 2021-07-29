6d1df56b7c263103bafaf992c93dcd1ddc3b03cd
Seme Mojugbe <sememojugbe@gmail.com>
2021-07-29 21:06:42 +0100

    majority of home screen minus masonry grid is done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+    </div> -->
+    <div class="flex mx-0 my-3 pb-3 px-6 header-box-shadhow bg-white">
+      <div>
+        <img src="https://via.placeholder.com/150x50" alt="">
+      </div>
+      <div class="spacer flex-grow"></div>
+      <ul class="flex items-center">
+        <li class="text-sm mx-2">
+          <a class="text-black poppins-font" href="#">Business Manager</a>
+        </li>
+        <li class="text-sm mx-2">
+          <a class="text-black poppins-font" href="#">Marketplace</a>
+        </li>
+        <li class="text-sm mx-2">
+          <a class="text-black poppins-font" href="#">XooCom</a>
+        </li>
+        <li class="text-sm mr-2 ml-32">
+          <a class="text-brand poppins-font" href="#">Join Peexoo</a>
+        </li>
+      </ul>
     </div>
     <router-view/>
   </div>
@@ -10,23 +30,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background: #FFFDF7;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,127 @@
 <template>
-  <div class="bg-red-500 py-12">
-    <div class="bg-white p-8 w-2/3 my-12 text-black mx-auto"></div>
+  <div>
+    <div class="rounded-md bg-gray-600 h-32 mx-3 banner-image">.</div>
+    <div class="flex mx-10">
+      <div class="w-1/3 bg-white border-brand border-1 rounded -mt-24">
+        <div class="text-right p-3 text-xs">
+          <button class="text-brand poppins-font">Hide</button>
+        </div>
+        <div class="text-center pb-8 px-8">
+          <img
+            class="w-24 h-24 inline-block"
+            src="@/assets/img/tim_mcbaj_53652000_2255623191378916_4966133143083590398_n.png"
+            alt=""
+          />
+          <h4 class="text-2xl font-semibold mt-3 mb-1 poppins-font">
+            Ekemini Mark
+          </h4>
+          <p class="text-xs leading-5 poppins-font">
+            I’m a wedding and portrait photographer based in Lagos, Nigeria.
+            I’ve been shooting weddings since I was in high school (before I
+            ever had my driver’s license!).
+          </p>
+          <div class="flex my-5">
+            <div class="poppins-font flex-1">
+              <h4 class="text-xl font-bold m-0">22.1k</h4>
+              <p class="text-xs m-0 text-gray-500">Total Views</p>
+            </div>
+            <div class="poppins-font flex-1">
+              <h4 class="text-xl font-bold m-0">
+                <img class="h-4 w-4 mx-1 inline-block" src="https://via.placeholder.com/150x50" />13.1k
+              </h4>
+              <p class="text-xs m-0 text-gray-500">Total Likes</p>
+            </div>
+            <div class="poppins-font flex-1">
+              <h4 class="text-xl font-bold m-0">Sony 92J</h4>
+              <p class="text-xs m-0 text-gray-500">Camera Lens</p>
+            </div>
+          </div>
+          <ul class="flex breadcrumbs bulleted poppins-font text-xs my-2">
+            <li>Category</li>
+            <li>Food</li>
+            <li>Portrait</li>
+            <li>Wedding</li>
+            <li>Model</li>
+          </ul>
+          <ul class="flex breadcrumbs poppins-font text-xs my-2">
+            <li class="fix">Location</li>
+            <li>Lagos, Nigeria</li>
+          </ul>
+          <div class="flex justify-center items-center mb-2 mt-8">
+            <img
+              class="h-4 w-4 mr-2"
+              src="https://via.placeholder.com/150x50"
+              alt=""
+            /><a href="#" class="poppins-font text-sm flex-initial"
+              >www.peexoo.ai/anijohnson</a
+            >
+          </div>
+          <div class="flex my-7 justify-center items-center">
+            <button class="icon-button flex-initial">
+              <img
+                class="h-4 w-4"
+                src="https://via.placeholder.com/150x50/000000"
+                alt=""
+              />
+            </button>
+            <button class="icon-button flex-initial">
+              <img
+                class="h-4 w-4"
+                src="https://via.placeholder.com/150x50/000000"
+                alt=""
+              />
+            </button>
+            <button class="icon-button flex-initial">
+              <img
+                class="h-4 w-4"
+                src="https://via.placeholder.com/150x50/000000"
+                alt=""
+              />
+            </button>
+            <a
+              href=""
+              class="
+                px-4
+                py-2
+                rounded-full
+                mx-1
+                bg-brand
+                poppins-font
+                text-white text-sm
+              "
+              >Book Now</a
+            >
+          </div>
+          <div class="text-center flex justify-center items-center text-xs poppins-font">
+            <span class="flex-initial poppins-font">Built</span>
+            <img
+              class="h-4 w-4 mx-1"
+              src="https://via.placeholder.com/150x50"
+              alt="" />
+            <span href="#" class="flex-initial poppins-font">
+               on peexoo
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="w-2/3 p-4">
+        <div class="block">
+          <button class="tab-button poppins-font active">Portfolio</button>
+          <button class="tab-button poppins-font">Availability</button>
+          <button class="tab-button poppins-font">Pricing Package</button>
+        </div>
+        <div class="flex text-sm">
+          <div class="px-3 py-2 poppins-font flex-grow text-right">
+            Sort by Image Category
+          </div>
+          <div
+            class="px-3 py-2 poppins-font flex-initial bg-brand rounded lighter"
+          >
+            xx
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +131,45 @@
 
 export default {
   name: 'Home',
-  components: {
-  }
+  components: {}
 }
 </script>
+
+<style lang="postcss" scoped>
+/* @import '../assets/shared.css' */
+.tab-button {
+  @apply py-2 px-5 rounded-full mr-2 text-sm;
+}
+.tab-button:hover,
+.tab-button.active {
+  background: #fef3e0;
+  @apply font-bold;
+}
+.breadcrumbs li {
+  position: relative;
+  @apply flex-initial pr-3 mr-2;
+}
+.breadcrumbs li:first-of-type {
+  @apply text-gray-400;
+}
+.breadcrumbs.bulleted li:last-of-type::before {
+  background: transparent;
+}
+.breadcrumbs li.fix {
+  @apply mr-3;
+}
+.breadcrumbs.bulleted li::before {
+  content: "";
+  position: absolute;
+  width: 5px;
+  height: 5px;
+  right: 0;
+  top: 6px;
+  background: black;
+  border-radius: 500px;
+}
+.icon-button {
+  background: #f7f7f7;
+  @apply p-3 rounded-full mx-1;
+}
+</style>
